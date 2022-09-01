72497fd2839a813c191ca8da40b939e058b137c6
Tomasz Mizak <tomasz.mizak99@gmail.com>
2022-09-01 14:55:48 +0200

    final version

--- ankieter.py
+++ ankieter.py
@@ -1,3 +1,4 @@
+from email import message
 from pprint import pprint
 from surveys import SurveyHelper
 import inquirer
@@ -7,11 +8,15 @@ from datetime import datetime
 from dbcontext import dbcontext
 from dotenv import dotenv_values
 
+# ---------------------- credits
+print('\n\n----------------------------------------')
+print("Ankieter v0.1\nMizak Tomasz")
+print('----------------------------------------\n')
+
 # ---------------------- initialization
 
 # load config
 config = dotenv_values('.env')
-erro = False
 
 # create None type varialbe for database context
 dbctx = None
@@ -59,7 +64,6 @@ finally:
 # finally create instance of SurveyHelper, using initialized dbcontext
 helper = SurveyHelper(dbctx)
 
-
 # ---------------------- inquirer prompting / final code
 all_surveys = [] # list for all survey codes available on '0500-%' filter.
 
@@ -82,26 +86,36 @@ while game_loop:
 
     if a1['type'] == 'Policz wyniki dla wszystkich':
 
-        workers = srv.workers(survey_code)
         result = []
-        wor_len = workers.shape[0]
-        print(f"Liczę średnią dla {wor_len} pracowników.")
-        pbar = tqdm(total=wor_len)
-        for i, r in workers.iterrows():
-            avg = srv.worker_avg(survey_code, r.prac_id)
-            result.append({
+        workers = helper.workers(survey_code)
+        print(f"Liczę średnią dla {workers.shape[0]} pracowników.")
+        pbar = tqdm(total=workers.shape[0]) # initialize progress bar
+        for i, r in workers.iterrows(): # iterrow workers from helper DataFrame
+            avg = helper.worker_avg(survey_code, r.prac_id) # use helper to calc worker avg
+            result.append({ # append worker to workers
                 "imie": r.imie,
                 "nazwisko": r.nazwisko,
                 "srednia": str(avg).replace('.', ',')
             })
-            pbar.update(1)
-        pbar.close()
+            pbar.update(1) # update progress bar progress
+        pbar.close() # close progress bar
+
+        # save result to file in main directory
+        # TODO: selecting path, where to save
         result = pd.DataFrame(result)
         now = datetime.now().strftime("%d-%m-%Y %H-%M-%S")
         filename = f"result {now}.csv"
 
-        result.to_csv(filename, sep=';', encoding='utf-8-sig')
-        print(f'Plik {filename} został zapisany w katalogu aplikacji.')
+        result.to_csv(filename, sep=';', encoding='utf-8-sig') # utf-8-sig (pl characters on utf-8 doesn't work)
+        print(f'Plik {filename} został zapisany w katalogu aplikacji.\n\n')
+
+        # continue program?
+        a2 = inquirer.prompt([
+            inquirer.Confirm("continue", message="Czy zacząć od początku?", default=False),
+        ])
+        
+        if not a2['continue']:
+            game_loop = False
 
     else:
         a2 = inquirer.prompt([
@@ -109,14 +123,28 @@ while game_loop:
         ])
         
         if a2['search-type'] == 'Wyświetl pełną listę pracowników na wybraną wcześniej ankietę':
-            workers = ['Chcę wrócić do poprzedniego wyboru']
+            workers = []
             for i, r in helper.workers(survey_code).iterrows():
                 workers.append(f'[{r.prac_id}] {r.imie} {r.nazwisko}')
 
             a3 = inquirer.prompt([
-                inquirer.List('search-type',message=f"Wybierz pracownika z ankiety {survey_code}",choices=workers)
+                inquirer.List('selected-worker',message=f"Wybierz pracownika z ankiety {survey_code}",choices=workers)
             ])
 
+            # read selection and splice selected string to get worker_id
+            selected = str(a3['selected-worker'])
+            worker_id = int(((selected.split(" ")[0]).replace('[', '')).replace(']', ''))
+            avg = helper.worker_avg(survey_code, worker_id)
+            print(f"Liczę średnią dla pracownika: {a3['selected-worker']}")
+            print(f"Średnia wynosi: {avg}\n\n")
+
+            # continue program?
+            a4 = inquirer.prompt([
+                inquirer.Confirm("continue", message="Czy zacząć od początku?", default=False),
+            ])
+            
+            if not a4['continue']:
+                game_loop = False
 
         else:
 
@@ -124,38 +152,33 @@ while game_loop:
 
             while condition:
 
-                questions = [
-                inquirer.Text('search-filter',
-                                message="Wpisz nazwisko",
-                            ),
-                ]
-                a3 = inquirer.prompt(questions)
+                a3 = inquirer.prompt([
+                    inquirer.Text('search-filter',message="Wpisz nazwisko (lub jego część, wielkość liter ma znaczenie)")
+                ])
 
-                print("Znalezione wiersze:")
-                pprint(srv.workers_filter(survey_code, a3['search-filter']))
+                workers = ['Użyj innego filtra']
+                for i, r in helper.workers_filter(survey_code, a3['search-filter']).iterrows():
+                    workers.append(f"[{r.prac_id}] {r.imie} {r.nazwisko}")
 
-                questions = [
-                inquirer.List('continue',
-                                message="Czy wyświetlona powyżej lista zawiera pracownika, którego szukasz?",
-                                choices=['Tak', 'Nie'],
-                            ),
-                ]
-                a4 = inquirer.prompt(questions)
+                a4 = inquirer.prompt([
+                    inquirer.List('selected-worker',message=f"Znaleziono następujących pracowników, użyto filtra: '{a3['search-filter']}'. Wybierz pracownika lub zacznij od nowa",choices=workers)
+                ])
+                
+                if not a4['selected-worker'] == 'Użyj innego filtra':
 
-                if a4['continue'] == 'Tak':
-                    questions = [
-                    inquirer.Text('worker_id',
-                                    message="W takim wypadku podaj kod tego pracownika, znajdujący się pod kolumną prac_id"
-                                ),
-                    ]
-                    a5 = inquirer.prompt(questions)
-                    
-                    print("Policzona średnia wynosi:")
-                    pprint(srv.worker_avg(survey_code, a5['worker_id']))
-                    print("\n")
+                    # read selection and splice selected string to get worker_id
+                    selected = str(a4['selected-worker'])
+                    worker_id = int(((selected.split(" ")[0]).replace('[', '')).replace(']', ''))
+                    avg = helper.worker_avg(survey_code, worker_id)
+                    print(f"Liczę średnią dla pracownika: {a4['selected-worker']}")
+                    print(f"Średnia wynosi: {avg}\n\n")
+
+                    # continue program?
+                    a4 = inquirer.prompt([
+                        inquirer.Confirm("continue", message="Czy zacząć od początku?", default=False),
+                    ])
 
                     condition = False
                     
-
-
-
+                    if not a4['continue']:
+                        game_loop = False
